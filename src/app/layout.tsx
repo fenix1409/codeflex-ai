7960696edf523211d927c0ec98ac96b80a4466fd
fenix1409 <b2550486@gmail.com>
2025-06-16 15:52:19 +0500

    ini

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,18 +27,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ConvexClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Navbar />
+
+          {/* GRID BACKGROUND */}
           <div className="fixed inset-0 -z-1">
             <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
             <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
           </div>
-          {children}
+
+          <main className="pt-24 flex-grow">{children}</main>
+          <Footer />
         </body>
       </html>
-    </ClerkProvider>
+    </ConvexClerkProvider>
   );
 }
